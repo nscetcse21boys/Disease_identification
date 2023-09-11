70178c97b49966a64e7e3cd2c5cf2956d4696dfc
E.SANTHOSH <115930011+Santhosh174@users.noreply.github.com>
2023-09-11 22:34:55 +0530

    stroke page

--- stroke.js
+++ stroke.js
@@ -1,54 +1,16 @@
-$('.button').click(function(){
-  var $btn = $(this),
-      $step = $btn.parents('.modal-body'),
-      stepIndex = $step.index(),
-      $pag = $('.modal-header span').eq(stepIndex);
+function ToggleCheckBox(elem) {
+  var TickLine1 = elem.querySelector(".tick>.Tickline1")
+  var Tickline2 = elem.querySelector(".tick>.Tickline2")
+  if (elem.getAttribute("data-status") == "true") {
+      TickLine1.style.opacity = 1
+      Tickline2.style.opacity = 1
+      elem.setAttribute("data-status", false)
 
-  if(stepIndex === 0 || stepIndex === 1) { step1($step, $pag); }
-  
-});
+  } else {
+      TickLine1.style.opacity = 0
+      Tickline2.style.opacity = 0
+      elem.setAttribute("data-status", true)
 
 
-function step1($step, $pag){
-console.log('step1');
-  // animate the step out
-  $step.addClass('animate-out');
-  
-  // animate the step in
-  setTimeout(function(){
-    $step.removeClass('animate-out is-showing')
-         .next().addClass('animate-in');
-    $pag.removeClass('is-active')
-          .next().addClass('is-active');
-  }, 600);
-  
-  // after the animation, adjust the classes
-  setTimeout(function(){
-    $step.next().removeClass('animate-in')
-          .addClass('is-showing');
-    
-  }, 1200);
+  }
 }
-
-
-function step3($step, $pag){
-console.log('3');
-
-  // animate the step out
-  $step.parents('.modal-wrap').addClass('animate-up');
-
-  setTimeout(function(){
-    $('.rerun-button').css('display', 'inline-block');
-  }, 300);
-}
-
-$('.rerun-button').click(function(){
- $('.modal-wrap').removeClass('animate-up')
-                  .find('.modal-body')
-                  .first().addClass('is-showing')
-                  .siblings().removeClass('is-showing');
-
-  $('.modal-header span').first().addClass('is-active')
-                          .siblings().removeClass('is-active');
- $(this).hide();
-});
